d79d070469d1f80733602e2536c2afb06967d0f2
karenlin7788 <xlin2@mail.csuchico.edu>
2017-10-12 10:01:01 -0700

    OldTimeyPhoto

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ cout<<"What is the name of the image?(BMP format image only)"<<endl;
 cin>>file;
 image.open(file);
 bmp=image.toPixelMatrix();
-cout<<"The size of the file is"<< bmp.size()<<"Pixel's height and "<<bmp[0].size()<< "Pixel's width"<<endl;
+cout<<"The size of the file is "<< bmp.size()<< "Pixel's height and" <<bmp[0].size()<<  "Pixel's width"<<endl;
 
 
 return 0;
@@ -27,5 +27,7 @@ return 0;
 
 
 //By using "cout", ask usere for the name they want to use for the file (must be a Windows BMP format image).
-//After getting the response from users, convert the image to make every pixel in grayscale.
-//After coverting the photo, ask users to save the file as oldtimey.bm
+//If the input of the user does not match the requirement, cout as"the file must be a 24 bit depth Windows BMP file, please enter the file name again". Use loop until we get the proper input".
+
+//After getting the response from users, save the original picture first and  convert the image to make every pixel in grayscale (take the average of three colors)
+//After coverting the photo, ask users to save the file as oldtimey.bmp
