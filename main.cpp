2343ef67a1a48e20adf14d2ded9f82fc7ebff777
karenlin7788 <xlin2@mail.csuchico.edu>
2017-10-12 16:23:02 -0700

    OldTimeyPhoto

--- main.cpp
+++ main.cpp
@@ -10,18 +10,50 @@ using namespace std;
 int main()
 {
 string file;
-
+int cols;
+int row;
 Bitmap image;
 vector<vector<Pixel> >bmp;
-Pixel rgb;
-bmp=image.toPixelMatrix();
-cout<<"What is the name of the image?(BMP format image only)"<<endl;
-cin>>file;
-image.open(file);
-bmp=image.toPixelMatrix();
-cout<<"The size of the file is "<< bmp.size()<< "Pixel's height and" <<bmp[0].size()<<  "Pixel's width"<<endl;
+bool validBmp;
+Pixel greyDot;
+do
+   {
+   cout<<"What is the name of the image?(BMP format image only)"<<endl;
+   cin>>file;
+   image.open(file);
+   validBmp = image.isImage();
+   if (validBmp != true)
+      {
+      cout<<"the file must be a 24 bit depth Windows BMP file, please ener the file name again";
+      }
 
+   }
+   while (validBmp != true);
+   bmp = image.toPixelMatrix();
+if (validBmp == true)
+      {
+   
+      cout<<"The size of the file is "<< bmp.size() << " Pixel's height and " <<bmp[0].size() <<  " Pixel's width"<<endl;
+      for (row=0;row<bmp.size();row++)
+         {
 
+          for (cols=0;cols<bmp[0].size();cols++)
+              {
+              greyDot=bmp[row][cols];
+
+              greyDot.red =0.299*255;
+              greyDot.green=0.587*255;
+              greyDot.blue=0.114*255;
+
+              bmp[row][cols]=greyDot;
+              }
+         }
+
+      
+      image.fromPixelMatrix(bmp);
+      image.save("OldTimeyPhot.bmp");
+      cout<<"Here is the OleTimeyPhoto: "<<file<<endl;
+      }
 return 0;
 }
 
@@ -31,3 +63,4 @@ return 0;
 
 //After getting the response from users, save the original picture first and  convert the image to make every pixel in grayscale (take the average of three colors)
 //After coverting the photo, ask users to save the file as oldtimey.bmp
+
